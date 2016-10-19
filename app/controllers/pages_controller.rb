137927a0e4d7817019ef28357c5f25b923aa58c0
Vivek <vivek.a.sivakumar@gmail.com>
2016-10-19 14:58:03 -0700

    add in a quick tag search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,18 @@
 class PagesController < ApplicationController
   def home
   	if signed_in?
-  		@event = current_user.events.build 
+  		@event = current_user.events.build
   		@feed_items = current_user.feed.paginate(page: params[:page])
   	end
+  end
+
+  def search
+    if signed_in?
+      @event = current_user.events.build 
+      @feed_items = Event.from_users_followed_by(current_user).tagged_with(params[:search]).paginate(page: params[:page])
+      @search_query = params[:search]
+      render 'pages/home'
+    end
   end
 
   def help
