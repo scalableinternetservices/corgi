288f17a08fa49ced40269091d222b90096d232aa
Vivek <vivek.a.sivakumar@gmail.com>
2016-11-03 20:05:31 -0700

    eliminate duplicates in explore page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,7 +10,6 @@ class PagesController < ApplicationController
     if signed_in?
       can_be_seen_events = Event.can_be_seen_events(current_user)
       @tag_results = can_be_seen_events.tagged_with(params[:search])
-      # @location_results = can_be_seen_events.tagged_with(params[:search])
       @location_results = can_be_seen_events.near(params[:search].to_s, 5)
       @user_results = User.where("user_name LIKE ?", "%#{params[:search]}%")
       @search_query = params[:search]
@@ -21,20 +20,25 @@ class PagesController < ApplicationController
     if signed_in?
       events_can_be_seen = Event.can_be_seen_events(current_user)
 
-      @explore_events = []
+      explore_event_ids = []
 
       # @events_top_likes: 10 events with top likes
-      @explore_events += events_can_be_seen.order(:likes_count).where.not(:user_id => current_user.id).first(10)
+      explore_event_ids += events_can_be_seen.order(:likes_count).where.not(:user_id => current_user.id).ids.first(10)
 
       # @events_with_same_tags: the events with same tags as current_user
       current_user.events.each do |event|
-        @explore_events += Event.tagged_with(event.tag_list, :any => true).where.not(:user_id => event.user.id)
+        explore_event_ids += Event.tagged_with(event.tag_list, :any => true).where.not(:user_id => event.user.id).ids
       end
 
       # @events_nearby: the events(created within 3 days till now) which are near to the current_user's events
+      # nearby_events = []
       current_user.events.where(:created_at => 3.days.ago..Time.now).each do |event|
-        @explore_events += events_can_be_seen.near(event.location, 100).where.not(:user_id => current_user.id)
+        nearby_events = events_can_be_seen.near(event.location, 50).where.not(:user_id => current_user.id)
+        explore_event_ids += (nearby_events.map {|event| event.id})
       end
+      
+
+      @explore_events = Event.where(:id => explore_event_ids).distinct
     end
   end
 
