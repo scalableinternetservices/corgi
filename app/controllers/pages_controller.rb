23dbe5c6742468b1652f2d436ec4bee8651cf659
Vivek <vivek.a.sivakumar@gmail.com>
2016-11-03 19:11:23 -0700

    some changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,40 +19,22 @@ class PagesController < ApplicationController
 
   def explore
     if signed_in?
-      @events_can_be_seen = Event.can_be_seen_events(current_user)
+      events_can_be_seen = Event.can_be_seen_events(current_user)
+
+      @explore_events = []
 
       # @events_top_likes: 10 events with top likes
-      @events_top_likes = @events_can_be_seen.order(:likes_count).first(10)
-
+      @explore_events += events_can_be_seen.order(:likes_count).where.not(:user_id => current_user.id).first(10)
 
       # @events_with_same_tags: the events with same tags as current_user
       current_user.events.each do |event|
-        event.tag_list.each do |tag|
-          if @events_with_same_tags
-            @events_with_same_tags = @events_with_same_tags.or(@events_can_be_seen.tagged_with(tag))
-          else
-            @events_with_same_tags = @events_can_be_seen.tagged_with(tag)
-          end
-        end
+        @explore_events += Event.tagged_with(event.tag_list, :any => true).where.not(:user_id => event.user.id)
       end
 
       # @events_nearby: the events(created within 3 days till now) which are near to the current_user's events
-      @events_recent = @events_can_be_seen.where(:created_at => 3.days.ago..Time.now) 
-      #@current_user_recent_events = current_user.events.where(:created_at => 7.days.ago..Time.now)
-=begin
-      @current_user_recent_events.each do |event|
-        if @events_nearby 
-          @events_nearby = @events_nearby.or(@events_recent.near(event.location, 100))
-        else
-          @events_nearby = @events_recent.near(event.location, 100)
-        end
+      current_user.events.where(:created_at => 3.days.ago..Time.now).each do |event|
+        @explore_events += events_can_be_seen.near(event.location, 100).where.not(:user_id => current_user.id)
       end
-=end
-      @events_nearby = @events_recent.near(current_user.events.first.location, 100)
-
-      # don't know how to union {@events_top_likes, @events_with_same_tags, @events_nearby} to @explore_events
-      @user = current_user
-      render 'pages/explore'
     end
   end
 
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,7 +5,7 @@ class ProfilesController < ApplicationController
     def show
       @user = User.find_by(user_name: params[:user_name])
       if @user
-        if @user = current_user or @user.isfriend(current_user)
+        if current_user?(@user) || @user.isfriend(current_user)
           @events = User.find_by(user_name: params[:user_name]).events.order('created_at DESC')
         else
           @events = User.find_by(user_name: params[:user_name]).events.where("isprivate = 0").order('created_at DESC')
