8cb2cc854d8a8ae6b8aa17b53158564c8ffe64c8
Vivek <vivek.a.sivakumar@gmail.com>
2016-11-07 22:58:34 -0800

    bug fix: view profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,7 +5,7 @@ class ProfilesController < ApplicationController
     def show
       @user = User.find_by(user_name: params[:user_name])
       if @user
-        if current_user?(@user) || @user.isfriend(current_user)
+        if current_user?(@user) || current_user.is_friend?(@user)
           @events = User.find_by(user_name: params[:user_name]).events.order('created_at DESC')
         else
           @events = User.find_by(user_name: params[:user_name]).events.where("isprivate = 0").order('created_at DESC')
